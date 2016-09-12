0cf5df9da3dc0a4f431f47ada47a9a5ba9097e11
leona <leona@leona.leona>
2016-09-12 18:09:41 +0800

    賽果最後產生

--- project_fuelphp/fuel/app/logs/2016/09/02.php
+++
@@ -1,4 +0,0 @@
-<?php defined('COREPATH') or exit('No direct script access allowed'); ?>
-
-WARNING - 2016-09-02 16:52:29 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
-WARNING - 2016-09-02 16:53:08 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
